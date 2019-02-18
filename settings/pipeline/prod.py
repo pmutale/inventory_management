a8b44e8a54ec2dac2b4bfb7e6917662d3eac0d8f
P. Mutale <peter@mutale.nl>
2019-02-18 16:29:03 +0100

    Release: Settings to webpack file

--- settings/pipeline/prod.py
+++ settings/pipeline/prod.py
@@ -7,7 +7,7 @@ DEBUG = False
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-prd.json'),
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-prod.json'),
     }
 }
 
