edc971422270979768e0379e79af9902209d4816
P. Mutale <peter@mutale.nl>
2019-02-18 16:08:23 +0100

    Release: Settings

--- settings/pipeline/heroku.py
+++ settings/pipeline/heroku.py
@@ -2,7 +2,7 @@ from settings.pipeline.prod import *
 
 ALLOWED_HOSTS = ['*']
 
-DEBUG = True
+DEBUG = False
 
 sentry_sdk.init(
     dsn="https://619102194c75442c9dd288ee8efe7144@sentry.io/1393371",
