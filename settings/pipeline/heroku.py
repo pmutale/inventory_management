84ac689a1c1d99769d0cf93f577763d10964a8ed
P. Mutale <peter@mutale.nl>
2019-02-18 22:09:49 +0100

    Removed static storage

--- settings/pipeline/heroku.py
+++ settings/pipeline/heroku.py
@@ -4,6 +4,8 @@ ALLOWED_HOSTS = ['*']
 
 DEBUG = False
 
+STATIC_ROOT = 'staticfiles'
+
 sentry_sdk.init(
     dsn="https://619102194c75442c9dd288ee8efe7144@sentry.io/1393371",
     integrations=[DjangoIntegration()]
